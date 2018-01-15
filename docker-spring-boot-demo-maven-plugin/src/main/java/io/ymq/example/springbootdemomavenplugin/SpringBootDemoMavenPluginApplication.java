392be74a526ccede5756c4852d1f2237efa91e51
闫鹏磊 <75997533@qq.com>
2018-01-15 17:14:12 +0800

    docker-spring-boot-demo-maven-plugin

---
+++ docker-spring-boot-demo-maven-plugin/src/main/java/io/ymq/example/springbootdemomavenplugin/SpringBootDemoMavenPluginApplication.java
@@ -0,0 +1,12 @@
+package io.ymq.example.springbootdemomavenplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoMavenPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoMavenPluginApplication.class, args);
+	}
+}
