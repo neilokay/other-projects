f975b0416bdc9fee4bb171a9fe30f071c347dbe4
闫鹏磊 <75997533@qq.com>
2018-01-15 19:12:54 +0800

    Docker 部署 SpringBoot 项目整合 Redis 镜像做访问计数Demo

--- docker-spring-boot-demo/src/main/java/io/ymq/example/springbootdemo/SpringBootDemoApplication.java
+++ docker-spring-boot-demo/src/main/java/io/ymq/example/springbootdemo/SpringBootDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 
 @RestController
